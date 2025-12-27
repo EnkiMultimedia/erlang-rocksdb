8bdc449dfca2f7de9885fdf10f37678d19b76b6b
Benoit Chesneau <bchesneau@gmail.com>
2025-12-27 23:51:02 +0100

    build snappy with RTTI enabled to match RocksDB

--- c_src/CMake/build-snappy.cmake
+++ c_src/CMake/build-snappy.cmake
@@ -9,12 +9,27 @@ set(SNAPPY_LIBRARY ${SNAPPY_STATIC_LIB})
 
 include(ExternalProject)
 
+# Build snappy with RTTI enabled to match RocksDB (which uses USE_RTTI=1)
+# Snappy's CMakeLists.txt strips -frtti and adds -fno-rtti to CMAKE_CXX_FLAGS.
+# We pass -frtti via CMAKE_CXX_FLAGS_RELEASE which gets appended AFTER base flags,
+# so the final order is "... -fno-rtti ... -frtti" and last flag wins with gcc/clang.
+if(MSVC)
+    set(SNAPPY_RTTI_FLAG "/GR")
+else()
+    set(SNAPPY_RTTI_FLAG "-frtti")
+endif()
+
 ExternalProject_Add(snappy
     SOURCE_DIR "${SNAPPY_SOURCE_DIR}"
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${SNAPPY_ROOT_DIR}
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+        -DCMAKE_BUILD_TYPE=Release
+        -DBUILD_SHARED_LIBS=OFF
+        -DSNAPPY_BUILD_TESTS=OFF
+        -DSNAPPY_BUILD_BENCHMARKS=OFF
+        -DCMAKE_CXX_FLAGS_RELEASE=${SNAPPY_RTTI_FLAG}
     BINARY_DIR ${SNAPPY_ROOT_DIR}
     BUILD_BYPRODUCTS "${SNAPPY_STATIC_LIB}"
     )
