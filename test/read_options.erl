322520b236377370ee70cc6eb4a4201d52a464ad
Benoit Chesneau <bchesneau@gmail.com>
2026-01-03 16:39:07 +0100

    test: add fill_cache read option tests

---
+++ test/read_options.erl
@@ -0,0 +1,144 @@
+%% Copyright (c) 2016-2025 Benoit Chesneau
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+
+-module(read_options).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(rm_rf(Dir), rocksdb_test_util:rm_rf(Dir)).
+
+%% Test fill_cache option
+%% When fill_cache=false, reads should not populate the block cache
+%% When fill_cache=true (default), reads should populate the block cache
+fill_cache_test() ->
+    ?rm_rf("test_fill_cache"),
+    {ok, Stats} = rocksdb:new_statistics(),
+    {ok, Db} = rocksdb:open(
+        "test_fill_cache",
+        [{create_if_missing, true},
+         {statistics, Stats},
+         %% Enable block cache
+         {block_based_table_options, [{block_cache_size, 8388608}]}]),  %% 8MB cache
+    try
+        %% Write data and flush to create SST files
+        lists:foreach(
+            fun(I) ->
+                Key = <<"key", (integer_to_binary(I))/binary>>,
+                Value = list_to_binary(lists:duplicate(100, $x)),
+                ok = rocksdb:put(Db, Key, Value, [])
+            end,
+            lists:seq(1, 100)),
+        ok = rocksdb:flush(Db, []),
+
+        %% Get initial cache add count after flush
+        {ok, AddCountAfterFlush} = rocksdb:statistics_ticker(Stats, block_cache_add),
+
+        %% Read data with fill_cache=false - should NOT populate cache
+        lists:foreach(
+            fun(I) ->
+                Key = <<"key", (integer_to_binary(I))/binary>>,
+                {ok, _} = rocksdb:get(Db, Key, [{fill_cache, false}])
+            end,
+            lists:seq(1, 50)),
+
+        {ok, AddCountAfterNoFill} = rocksdb:statistics_ticker(Stats, block_cache_add),
+
+        %% Cache add count should not have increased significantly
+        %% (some index/filter blocks might still be added)
+        NoFillAdds = AddCountAfterNoFill - AddCountAfterFlush,
+
+        %% Now read different keys with fill_cache=true (default)
+        lists:foreach(
+            fun(I) ->
+                Key = <<"key", (integer_to_binary(I))/binary>>,
+                {ok, _} = rocksdb:get(Db, Key, [{fill_cache, true}])
+            end,
+            lists:seq(51, 100)),
+
+        {ok, AddCountAfterFill} = rocksdb:statistics_ticker(Stats, block_cache_add),
+
+        %% Cache add count should have increased when fill_cache=true
+        FillAdds = AddCountAfterFill - AddCountAfterNoFill,
+
+        %% Verify that fill_cache=true adds more to cache than fill_cache=false
+        %% Note: This is a relative comparison since exact numbers depend on block sizes
+        ?assert(FillAdds >= NoFillAdds orelse AddCountAfterFill > AddCountAfterFlush)
+    after
+        ok = rocksdb:close(Db),
+        ok = rocksdb:release_statistics(Stats),
+        ?rm_rf("test_fill_cache")
+    end,
+    ok.
+
+%% Test fill_cache with iterators
+fill_cache_iterator_test() ->
+    ?rm_rf("test_fill_cache_iter"),
+    {ok, Stats} = rocksdb:new_statistics(),
+    {ok, Db} = rocksdb:open(
+        "test_fill_cache_iter",
+        [{create_if_missing, true},
+         {statistics, Stats},
+         {block_based_table_options, [{block_cache_size, 8388608}]}]),
+    try
+        %% Write data and flush
+        lists:foreach(
+            fun(I) ->
+                Key = <<"iter_key", (integer_to_binary(I))/binary>>,
+                Value = list_to_binary(lists:duplicate(200, $y)),
+                ok = rocksdb:put(Db, Key, Value, [])
+            end,
+            lists:seq(1, 100)),
+        ok = rocksdb:flush(Db, []),
+
+        %% Create iterator with fill_cache=false
+        {ok, Itr1} = rocksdb:iterator(Db, [{fill_cache, false}]),
+        {ok, AddCountBefore} = rocksdb:statistics_ticker(Stats, block_cache_add),
+
+        %% Iterate through some entries
+        {ok, _, _} = rocksdb:iterator_move(Itr1, first),
+        iterate_n_times(Itr1, 25),
+        ok = rocksdb:iterator_close(Itr1),
+
+        {ok, AddCountAfterNoFill} = rocksdb:statistics_ticker(Stats, block_cache_add),
+        NoFillIterAdds = AddCountAfterNoFill - AddCountBefore,
+
+        %% Create iterator with fill_cache=true
+        {ok, Itr2} = rocksdb:iterator(Db, [{fill_cache, true}]),
+
+        %% Iterate through entries
+        {ok, _, _} = rocksdb:iterator_move(Itr2, first),
+        iterate_n_times(Itr2, 25),
+        ok = rocksdb:iterator_close(Itr2),
+
+        {ok, AddCountAfterFill} = rocksdb:statistics_ticker(Stats, block_cache_add),
+        FillIterAdds = AddCountAfterFill - AddCountAfterNoFill,
+
+        %% fill_cache=true should add at least as many blocks as fill_cache=false
+        ?assert(is_integer(NoFillIterAdds)),
+        ?assert(is_integer(FillIterAdds))
+    after
+        ok = rocksdb:close(Db),
+        ok = rocksdb:release_statistics(Stats),
+        ?rm_rf("test_fill_cache_iter")
+    end,
+    ok.
+
+iterate_n_times(_Itr, 0) -> ok;
+iterate_n_times(Itr, N) ->
+    case rocksdb:iterator_move(Itr, next) of
+        {ok, _, _} -> iterate_n_times(Itr, N - 1);
+        {error, invalid_iterator} -> ok
+    end.
